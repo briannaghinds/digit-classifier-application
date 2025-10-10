c4f227e4f362c0b86f2c902dab0fd1bfb2011f18
Brianna Hinds <briden32016@gmail.com>
2025-10-10 14:06:55 -0500

    fixed minor errors

--- model/utilities.py
+++ model/utilities.py
@@ -14,7 +14,7 @@ def train_model(model, training_data, epochs, optimizer, loss_fuc, device):
             optimizer.step()
             
         if batch_i % 20 == 0:
-            print(f"Train Epoch: {epoch} [{batch_i * len(data)}/{len(training_data.dataset)} ({100. * batch_i / len(training_data):.0f}%)]\tLoss: {loss.item():.6f}")
+            print(f"Train Epoch: {epoch} [{batch_i * len(data)}/{len(training_data.dataset)} ({100. * batch_i / len(training_data.dataset):.0f}%)]\tLoss: {loss.item():.6f}")
 
 
 def test_model(model, testing_data, loss_fuc, device):
@@ -26,9 +26,9 @@ def test_model(model, testing_data, loss_fuc, device):
         for data, target in testing_data:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += loss_fuc(output, target).item()
+            test_loss += loss_fuc(output, target).item() * data.size(0)
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(testing_data)
-    print(f"\nTest Set: Average Loss: {test_loss:.4f}, Accuracy: {correct}/{len(testing_data)} ({100. * correct / len(testing_data):.0f}%\n)")
+    print(f"\nTest Set: Average Loss: {test_loss:.4f}, Accuracy: {correct}/{len(testing_data.dataset)} ({100. * correct / len(testing_data.dataset):.0f}%\n)")
